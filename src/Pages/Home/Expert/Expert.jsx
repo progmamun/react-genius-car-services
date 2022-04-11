a60cc3284a4ab302d3088c3a8a81531ab50f1071
progmamun <mamun333089@gmail.com>
2022-04-11 06:53:05 +0600

    responsive-banner

--- src/Pages/Home/Expert/Expert.jsx
+++ src/Pages/Home/Expert/Expert.jsx
@@ -3,13 +3,12 @@ import React from 'react';
 const Expert = ({ expert }) => {
   const { name, img } = expert;
   return (
-    <div
-      className="card text-dark bg-info mb-3 col-sm-12 col-md-6 col-lg-4 gx-5"
-      style={{ width: '18rem' }}
-    >
-      <div className="card-body">
-        <img src={img} alt="" />
-        <h5 className="card-title">{name}</h5>
+    <div className="mb-3 col-sm-12 col-md-6 col-lg-4 g-5">
+      <div className="card text-dark" style={{ width: '18rem' }}>
+        <div className="card-body">
+          <img src={img} alt="" />
+          <h5 className="card-title">{name}</h5>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -13,7 +13,7 @@ const Services = () => {
 
   return (
     <div>
-      <h2 className="services-title">Services: </h2>
+      <h2 className="services-title mt-5">Services</h2>
       <div className="services-container">
         {services.map(service => (
           <Service key={service.id} service={service}></Service>
