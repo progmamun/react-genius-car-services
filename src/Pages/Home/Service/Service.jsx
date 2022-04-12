001a53917f1ea421fdfe051fd646a0742acf17e2
progmamun <mamun333089@gmail.com>
2022-04-13 05:16:49 +0600

    dynamic-route

--- src/Pages/Home/Service/Service.jsx
+++ src/Pages/Home/Service/Service.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Service.css';
 
 const Service = ({ service }) => {
-  const { name, img, description, price } = service;
+  const { id, name, img, description, price } = service;
+
+  const navigate = useNavigate();
+  const navigateToServiceDetail = id => {
+    navigate(`/service/${id}`);
+  };
   return (
     <div className="service">
       <img className="w-100" src={img} alt="" />
@@ -11,7 +17,12 @@ const Service = ({ service }) => {
       <p>
         <small>{description}</small>
       </p>
-      <button className="btn btn-info">Book: {name}</button>
+      <button
+        className="btn btn-info"
+        onClick={() => navigateToServiceDetail(id)}
+      >
+        Book: {name}
+      </button>
     </div>
   );
 };
