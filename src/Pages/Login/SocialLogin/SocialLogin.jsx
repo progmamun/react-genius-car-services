0a55bbcae15405c60257d89acc49113c18a4ff09
progmamun <mamun333089@gmail.com>
2022-04-14 05:47:22 +0600

    social-logo

--- src/Pages/Login/SocialLogin/SocialLogin.jsx
+++ src/Pages/Login/SocialLogin/SocialLogin.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Google from '../../../images/social/google-icon.png';
+import Fb from '../../../images/social/fb-logo.png';
+import Git from '../../../images/social/25231.png';
 
 const SocialLogin = () => {
   return (
@@ -9,7 +12,20 @@ const SocialLogin = () => {
         <div style={{ height: '1px' }} className="w-50 bg-warning"></div>
       </div>
 
-      <button className='w-50 p-3 btn btn-info'>Google Sign In</button>
+      <div className="">
+        <button className="w-50 p-3 btn btn-info d-block mx-auto">
+          <img src={Google} alt="" />
+          <span className="px-2">Google Sign In</span>
+        </button>
+        <button className="w-50 p-3 btn btn-info d-block mx-auto mt-2">
+          <img style={{ width: '30px' }} src={Fb} alt="" />
+          <span className="px-2">Google Sign In</span>
+        </button>
+        <button className="w-50 p-3 btn btn-info d-block mx-auto mt-2">
+          <img style={{ width: '30px' }} src={Git} alt="" />
+          <span className="px-2">Google Sign In</span>
+        </button>
+      </div>
     </div>
   );
 };
