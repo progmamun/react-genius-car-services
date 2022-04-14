974026d37770b1511bb5499db1b0f582a1c9e8f9
progmamun <mamun333089@gmail.com>
2022-04-14 12:44:45 +0600

    verify-email-async-await

--- src/Pages/Login/Register/Register.jsx
+++ src/Pages/Login/Register/Register.jsx
@@ -1,31 +1,39 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import {
+  useCreateUserWithEmailAndPassword,
+  useUpdateProfile,
+} from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import { async } from '@firebase/util';
 
 const Register = () => {
   const [agree, setAgree] = useState(false);
   const [createUserWithEmailAndPassword, user, loading, error] =
-    useCreateUserWithEmailAndPassword(auth);
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+  const [updateProfile, updating, error1] = useUpdateProfile(auth);
 
   const navigate = useNavigate();
   const navigateLogin = () => {
     navigate('/login');
   };
-  if (user) {
-    navigate('/home');
-  }
-  const handleRegister = event => {
+  // if (user) {
+  //   navigate('/home');
+  // }
+  const handleRegister = async event => {
     event.preventDefault();
     const name = event.target.name.value;
     const email = event.target.email.value;
     const password = event.target.password.value;
     // const agree = event.target.terms.checked;
-    if (agree) {
-      createUserWithEmailAndPassword(email, password);
-    }
+    // if (agree) {
+    //   createUserWithEmailAndPassword(email, password);
+    // }
+    await createUserWithEmailAndPassword(email, password);
+    await updateProfile({ displayName: name });
+    navigate('/home');
   };
   return (
     <div className="register-form">
