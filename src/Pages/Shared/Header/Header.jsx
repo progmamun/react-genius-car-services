a00e5dd1f4445971a3f102c18fa13095e794199f
progmamun <mamun333089@gmail.com>
2022-04-23 19:57:57 +0600

    manage-services-delete-api

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -48,6 +48,16 @@ const Header = () => {
               <Nav.Link as={Link} to="/about">
                 About
               </Nav.Link>
+              {user && (
+                <>
+                  <Nav.Link as={Link} to="/addservice">
+                    Add Service
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/manage">
+                    Manage
+                  </Nav.Link>
+                </>
+              )}
               {user ? (
                 <button
                   className="btn btn-link text-decoration-none"
