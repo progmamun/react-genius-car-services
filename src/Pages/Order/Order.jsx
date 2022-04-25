6142d074dfde8bbdaf8f5aec5f0e20f09ed90f14
progmamun <mamun333089@gmail.com>
2022-04-25 16:34:32 +0600

    handle-interceptors-401-403

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -2,19 +2,28 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import auth from '../../firebase.init';
 import useAuthState from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+import axiosPrivate from '../../api/axiosPrivate';
 
 const Order = () => {
   const [user] = useAuthState(auth);
   const [orders, setOrders] = useState([]);
+  const navigate = useNavigate();
+
   useEffect(() => {
     const getOrders = async () => {
       const url = `http://localhost:5000/order`;
-      const { data } = await axios.get(url, {
-        headers: {
-          authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-        },
-      });
-      setOrders(data);
+      try {
+        const { data } = await axiosPrivate.get(url);
+        setOrders(data);
+      } catch (error) {
+        console.log(error.message);
+        if (error.response.status === 401 || error.response.status === 403) {
+          signOut(auth);
+          navigate('/login');
+        }
+      }
     };
     getOrders();
   }, []);
